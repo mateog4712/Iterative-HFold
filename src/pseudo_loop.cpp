e836ddf792ca1288f1c8098d22735b85543b548a
Mateo Gray <mateo2@Mateos-Mac-Studio.local>
2025-08-25 09:06:07 -0600

    Fixed bug in BE unpaired base calculation, fixed bug in VP minimization of case 6

--- src/pseudo_loop.cpp
+++ src/pseudo_loop.cpp
@@ -295,7 +295,7 @@ void pseudo_loop::compute_VP(cand_pos_t i, cand_pos_t j, sparse_tree &tree){
 		cand_pos_t max_i_bp = std::max(tree.B(i,j),tree.bp(i,j));
 
 		for(cand_pos_t k = i+1; k<min_Bp_j; ++k){
-			m6 = get_WIP(i+1,k-1) + get_VP(k,j-1);
+			m6 = std::min(m6,get_WIP(i+1,k-1) + get_VP(k,j-1));
 		}
 		
 		m6 += ap_penalty + 2*bp_penalty;
@@ -550,7 +550,7 @@ void pseudo_loop::compute_BE(cand_pos_t i, cand_pos_t j, cand_pos_t ip, cand_pos
 				// Hosna: July 5th, 2007
 				// After meeting with Anne and Cristina --> ap should have 2* bp to consider the biggest and the one that crosses
 				// in a multiloop that spans a band
-				energy_t tmp = get_WIP(i+1,l-1) + get_BE(l,lp,ip,jp,tree) + cp_penalty * (j-lp+1) + ap_penalty + 2*bp_penalty;
+				energy_t tmp = get_WIP(i+1,l-1) + get_BE(l,lp,ip,jp,tree) + cp_penalty * (j-lp-1) + ap_penalty + 2*bp_penalty;
 				m4 = std::min(m4,tmp);
 			}
 
@@ -559,7 +559,7 @@ void pseudo_loop::compute_BE(cand_pos_t i, cand_pos_t j, cand_pos_t ip, cand_pos
 				// Hosna: July 5th, 2007
 				// After meeting with Anne and Cristina --> ap should have 2* bp to consider the biggest and the one that crosses
 				// in a multiloop that spans a band
-				energy_t tmp = ap_penalty + 2*bp_penalty + (cp_penalty * (l-i+1)) + get_BE(l,lp,ip,jp,tree) + get_WIP(lp+1,j-1);
+				energy_t tmp = ap_penalty + 2*bp_penalty + (cp_penalty * (l-i-1)) + get_BE(l,lp,ip,jp,tree) + get_WIP(lp+1,j-1);
 				m5 = std::min(m5,tmp);
 			}
 		}
@@ -1336,7 +1336,7 @@ void pseudo_loop::back_track(std::string structure, minimum_fold *f, seq_interva
 					// Hosna: July 5th, 2007
 					// After meeting with Anne and Cristina --> ap should have 2* bp to consider the biggest and the one that crosses
 					// in a multiloop that spans a band
-					tmp = get_WIP(i+1,l-1) + get_BE(l,lp,ip,jp,tree) + c_penalty * (j-lp+1) + ap_penalty + 2* bp_penalty;
+					tmp = get_WIP(i+1,l-1) + get_BE(l,lp,ip,jp,tree) + c_penalty * (j-lp-1) + ap_penalty + 2* bp_penalty;
 					if (min > tmp){
 						min = tmp;
 						best_row = 4;
@@ -1349,7 +1349,7 @@ void pseudo_loop::back_track(std::string structure, minimum_fold *f, seq_interva
 					// Hosna: July 5th, 2007
 					// After meeting with Anne and Cristina --> ap should have 2* bp to consider the biggest and the one that crosses
 					// in a multiloop that spans a band
-					tmp = ap_penalty + 2* bp_penalty+ c_penalty * (l-i+1) + get_BE(l,lp,ip,jp,tree) + get_WIP(lp+1,j-1);
+					tmp = ap_penalty + 2* bp_penalty+ c_penalty * (l-i-1) + get_BE(l,lp,ip,jp,tree) + get_WIP(lp+1,j-1);
 					if (min > tmp){
 						min = tmp;
 						best_row = 5;
